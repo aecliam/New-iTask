48abf65467aeb638566009a66af1b5848195efbd
aecliam <84498328+aecliam@users.noreply.github.com>
2023-09-08 21:00:34 +0800

    Create task-list.php

---
+++ task-list.php
@@ -0,0 +1,71 @@
+<div>
+	<div>
+		<div>
+			<div>
+				<a href="./index.php?page=new-project"><i></i> Add New project </a>
+			</div>
+		</div>
+		<div>
+			<table id="list">
+				<colgroup>
+					<col width="5%">
+					<col width="15%">
+					<col width="20%">
+					<col width="15%">
+					<col width="15%">
+					<col width="10%">
+					<col width="10%">
+					<col width="10%">
+				</colgroup>
+				<thead>
+					<tr>
+						<th> # </th>
+						<th> Project </th>
+						<th> Task </th>
+						<th> Project Started </th>
+						<th> Project Due Date </th>
+						<th> Project Status </th>
+						<th> Task Status </th>
+						<th> Action </th>
+					</tr>
+				</thead>
+				<tbody>
+					
+					<tr>
+						<td</td>
+						<td>
+							<p><b></b></p>
+						</td>
+						<td>
+							<p><b></b></p>
+							<p></p>
+						</td>
+						<td><b></b></td>
+						<td><b></b></td>
+						<td >
+							
+						</td>
+						<td>
+                        	
+                        </td>
+						<td >
+							<button type="button"> Action </button>
+			                    <div>
+			                      <a> Add Productivity </a>
+								</div>
+						</td>
+					</tr>	
+				
+				</tbody>
+			</table>
+		</div>
+	</div>
+</div>
+<style>
+	table p{
+		margin: unset !important;
+	}
+	table td{
+		vertical-align: middle !important
+	}
+</style>
