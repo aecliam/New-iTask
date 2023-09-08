28165cad64e1083040046cf59c9a7b4a4a2831a1
aecliam <84498328+aecliam@users.noreply.github.com>
2023-09-08 21:00:31 +0800

    Create new-project.php

---
+++ new-project.php
@@ -0,0 +1,84 @@
+<div>
+	<div>
+		<div>
+			<form action="" id="manage-project">
+
+        <input type="hidden" name="id">
+		<div>
+			<div>
+				<div>
+					<label for=""> Name </label>
+					<input type="text" name="name">
+				</div>
+			</div>
+
+          	<div>
+				<div>
+					<label for=""> Status </label>
+					    <select name="status" id="status">
+						    <option value="0"> Pending </option>
+						    <option value="3"> On-Hold </option>
+						    <option value="5"> Done </option>
+					    </select>
+				</div>
+			</div>
+		</div>
+
+		<div>
+			<div>
+            <div>
+              <label for=""> Start Date </label>
+              <input type="date" autocomplete="off" name="start_date">
+            </div>
+          </div>
+
+          <div>
+            <div>
+              <label for=""> End Date </label>
+              <input type="date" autocomplete="off" name="end_date">
+            </div>
+          </div>
+		</div>
+
+        <div>
+           <div>
+            <div>
+              <label for=""> Project Manager </label>
+              <select name="manager_id">
+              	<option></option>
+            
+              	<option></option>
+
+              </select>
+            </div>
+          </div>
+
+      	<input type="hidden" name="manager_id">
+
+          <div>
+            <div>
+              <label for=""> Project Team Members </label>
+              
+            </div>
+          </div>
+        </div>
+		<div>
+			<div>
+				<div>
+					<label for=""> Description </label>
+					<textarea name="description" id="" cols="30" rows="10" class="summernote form-control">
+						
+					</textarea>
+				</div>
+			</div>
+		</div>
+        </form>
+    	</div>
+    	<div>
+    		<div>
+    			<button form="manage-project"> Save </button>
+    			<button type="button" onclick="location.href='index.php?page=project_list'"> Cancel </button>
+    		</div>
+    	</div>
+	</div>
+</div>
