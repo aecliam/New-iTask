a8dab52b3b6108b65b13a1ebfb7d9c27a594eca4
aecliam <84498328+aecliam@users.noreply.github.com>
2023-09-09 18:10:09 +0800

    Update index-new.php

--- index-new.php
+++ index-new.php
@@ -9,16 +9,17 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
 
-    <body>
+    <body class="home-section">
         <div class="sidebar open">
             <div class="logo-details">
                 <i class='bx bx-menu'></i>
-                <span class="logo-name"> TYDO </span>
+                <a href="index-new.php">
+                <span class="logo-name" > TYDO </span></a>
             </div>
 
             <ul class="nav-links">
                 <li>
-                    <a href="index.php">
+                    <a href="index-new.php">
                         <i class='bx bx-grid-alt'></i>
                         <span class="link-name"> Dashboard </span>
                     </a>                   
@@ -27,39 +28,39 @@
                 <hr> 
                     <li>
                         <div class="iocn-link">
-                            <a href="./index.php?page=project_list">
+                            <a href="./index-new.php?page=project_list">
                                 <i class='bx bx-collection'></i>
-                                <span class="link-name" href="./index.php?page=project_list"> Projects </span>
+                                <span class="link-name" href="./index-new.php?page=project_list"> Projects </span>
                             </a>
                         </div>
 
                     </li>
 
                     <li>
-                        <a href="./index.php?page=task_list">
+                        <a href="./index-new.php?page=task_list">
                             <i class='bx bx-pin'></i>
-                            <span class="link-name"> Tasks </span>
+                            <span class="link-name" href="./index-new.php?page=tasks"> Tasks </span>
                         </a>
 
                     </li>
 
                     <li>
-                        <a href="./index.php?page=scholars">
+                        <a href="./index-new.php?page=scholars">
                             <i class='bx bx-spreadsheet'></i>
-                            <span class="link-name" href="./index.php?page=scholars"> Scholars </span>
+                            <span class="link-name" href="./index-new.php?page=scholars"> Scholars </span>
                         </a>
 
                     </li>
 
                     <li>
-                        <a href="./index.php?page=calendar">
+                        <a href="./index-new.php?page=calendar">
                             <i class='bx bx-calendar'></i>
-                            <span class="link-name" href="./index.php?page=calendar"> Calendar </span>
+                            <span class="link-name" href="./index-new.php?page=calendar"> Calendar </span>
                         </a>
                     </li>
 
                     <li>
-                        <a href="./index.php?page=inbox">
+                        <a href="./index-new.php?page=inbox">
                             <i class='bx bx-message'></i>
                             <span class="link-name">Inbox</span>
                         </a>
@@ -121,8 +122,6 @@
                 sidebar.classList.toggle("close");
             });
         </script>
-
-        
 
     </body>
 </html>
