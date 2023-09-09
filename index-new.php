242f871e65a0c91cd34a0b881d30233de56038f9
aecliam <84498328+aecliam@users.noreply.github.com>
2023-09-09 17:30:41 +0800

    Create index-new.php

---
+++ index-new.php
@@ -0,0 +1,128 @@
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="UTF-8">
+        <!--<title>  </title>-->
+        <link rel="stylesheet" type="text/css" href="css/index-new.css"/>
+
+        <!-- Boxiocns CDN Link -->
+        <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+
+    <body>
+        <div class="sidebar open">
+            <div class="logo-details">
+                <i class='bx bx-menu'></i>
+                <span class="logo-name"> TYDO </span>
+            </div>
+
+            <ul class="nav-links">
+                <li>
+                    <a href="index.php">
+                        <i class='bx bx-grid-alt'></i>
+                        <span class="link-name"> Dashboard </span>
+                    </a>                   
+                </li>
+
+                <hr> 
+                    <li>
+                        <div class="iocn-link">
+                            <a href="./index.php?page=project_list">
+                                <i class='bx bx-collection'></i>
+                                <span class="link-name" href="./index.php?page=project_list"> Projects </span>
+                            </a>
+                        </div>
+
+                    </li>
+
+                    <li>
+                        <a href="./index.php?page=task_list">
+                            <i class='bx bx-pin'></i>
+                            <span class="link-name"> Tasks </span>
+                        </a>
+
+                    </li>
+
+                    <li>
+                        <a href="./index.php?page=scholars">
+                            <i class='bx bx-spreadsheet'></i>
+                            <span class="link-name" href="./index.php?page=scholars"> Scholars </span>
+                        </a>
+
+                    </li>
+
+                    <li>
+                        <a href="./index.php?page=calendar">
+                            <i class='bx bx-calendar'></i>
+                            <span class="link-name" href="./index.php?page=calendar"> Calendar </span>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="./index.php?page=inbox">
+                            <i class='bx bx-message'></i>
+                            <span class="link-name">Inbox</span>
+                        </a>
+                    </li>
+
+                    <!--li>
+                        <a href="./index.php?page=conference">
+                            <i class='bx bxs-video'></i>
+                            <span class="link_name">Conference</span>
+                        </a>
+                            
+                    </li-->
+                </hr>
+                
+                <hr>
+                    <li>
+                        <div class="iocn-link">
+                            <a href="./index.php?page=user_list">
+                                <i class='bx bx-user'></i>
+                                <span class="link-name" href="./index.php?page=user_list"> Users </span>
+                            </a> 
+                        </div>
+                    </li>
+
+                    <li>
+                        <a href="./index.php?page=archive">
+                            <i class='bx bx-archive'></i>
+                            <span class="link-name"> Archives </span>
+                        </a>
+
+                    </li>
+                
+                    <!-- logout-->
+                    
+                    <li>
+                        <div class="logout">
+                            <a href="login.php">
+                                <i class='bx bx-log-out'></i>
+                                <span class="link-name"> Logout </span>
+                            </a>
+                        </div>
+                    </li>
+                </hr>
+            </ul>
+        </div>                    
+        
+        <script>
+            let arrow = document.querySelectorAll(".arrow");
+            for (var i = 0; i < arrow.length; i++) {
+                arrow[i].addEventListener("click", (e)=>{
+                let arrowParent = e.target.parentElement.parentElement;//selecting main parent of arrow
+                arrowParent.classList.toggle("showMenu");
+                    });
+            }
+            let sidebar = document.querySelector(".sidebar");
+            let sidebarBtn = document.querySelector(".bx-menu");
+            console.log(sidebarBtn);
+            sidebarBtn.addEventListener("click", ()=>{
+                sidebar.classList.toggle("close");
+            });
+        </script>
+
+        
+
+    </body>
+</html>
