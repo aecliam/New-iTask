fd385057ffa61711310f3d8c6ea4dcbdbf472178
aecliam <84498328+aecliam@users.noreply.github.com>
2023-09-19 22:36:46 +0800

    up

--- header.php
+++ header.php
@@ -1,11 +1,11 @@
 <div class="text header text-align">
-                    <form method="POST" action="admin_reservation_logs.php">
-                        <input type="search" name="search" placeholder="Search here">
-                        <input type="submit" name="submit" value="Search">
-                    </form>
+    <!-- <form method="POST" action="admin_reservation_logs.php">
+        <input type="search" name="search" placeholder="Search here">
+        <input type="submit" name="submit" value="Search">
+    </form>-->
 
-                    <a href="user-profile.php">
-                        <i class='bx bx-user'></i>
-                        <span class="link-name" href="user-profile.php"> User Account </span> <!-- change to user account, fetch from db -->
-                    </a>
-                </div>
+    <a href="user-profile.php">
+        <i class='bx bx-user'></i>
+        <span class="link-name" href="user-profile.php"> User Account </span> <!-- change to user account, fetch from db -->
+    </a>
+</div>
