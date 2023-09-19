2d6c137ee8c84e476a5976497c9465667079c530
aecliam <84498328+aecliam@users.noreply.github.com>
2023-09-19 15:09:12 +0800

    Add files via upload

---
+++ project-list.php
@@ -0,0 +1,13 @@
+
+
+<table>
+    <tr>
+        <th> #
+        <th> Project Name
+        <th> Start Date
+        <th> End Date
+        <th> Status
+        <th> Priority
+        <th> Action    
+    </tr>
+</table>
